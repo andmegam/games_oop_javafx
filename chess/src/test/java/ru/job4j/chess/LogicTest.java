258c7d0be4565866e10fde5d66d0f20df412ab83
andmmm <andmmm@mail.ru>
2021-12-20 21:43:15 +0400

    Каркас шахматной доски [#792] fix 1

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -9,7 +9,8 @@ import static org.junit.Assert.*;
 public class LogicTest {
 
     @Test
-    public void whenMoveWithoutEx() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void whenMoveWithoutEx()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H6);
@@ -18,14 +19,16 @@ public class LogicTest {
     }
 
     @Test(expected = FigureNotFoundException.class)
-    public void whenMoveWithFigureNotFoundEx() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void whenMoveWithFigureNotFoundEx()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C2, Cell.D3);
     }
 
     @Test(expected = OccupiedCellException.class)
-    public void whenMoveWithOccupiedCellEx() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void whenMoveWithOccupiedCellEx()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.add(new BishopBlack(Cell.E3));
@@ -33,7 +36,8 @@ public class LogicTest {
     }
 
     @Test(expected = ImpossibleMoveException.class)
-    public void whenMoveWithImpossibleMoveEx() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void whenMoveWithImpossibleMoveEx()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.E4);
